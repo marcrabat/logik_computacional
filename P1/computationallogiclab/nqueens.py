43a09adcfdcc3906aa44caa3ea86c1e9205c59bc
mrabatp <marc.rabat01@estudiant.upf.edu>
2017-05-14 19:26:51 +0200

    Working Perfectly!

--- P1/computationallogiclab/nqueens.py
+++ P1/computationallogiclab/nqueens.py
@@ -8,47 +8,34 @@ def nqueens(n):
     '''
     This function uses the other functions implemented above and returns a CNF formula representing the n queens problem.
     '''
-    solution = " "
+    solution = ""
     board=(np.array(range(int(n)*int(n)))+1).reshape(int(n),int(n))
+    
     for i in range(1,n+1):
         for j in range(1,n+1):
             solution += str(((i-1)*n)+j) + " "
         solution += "0 \n"
-        
+    
+    for j in range(1,n+1):
+        for i in range(1,n+1):
+            solution += str(((i-1)*n)+j) + " "
+        solution += "0\n"
+
     boardT = board.T
+
     for i in range(0,n): #lineas
         solution += writeCNF(board[i])
     for i in range(0,n): #columnas
         solution += writeCNF(boardT[i])
     tmp = createDiagonals(boardT)
     tmp.sort(key=len,reverse=True)
+    '''
     print boardT
     print tmp
+    '''
     for diagonal in tmp:#createDiagonals(boardT):
         solution += writeCNF(diagonal)
     return solution
-
-"""
-def nQueenProblem( n ):
-  queens = [0]*n
-  addQueen( 0, queens, n )
-  return queens
- 
-def addQueen( x, queens, n ):
-  i = 0
-  while i < n and 0 == queens[n - 1]:
-    if safeToAdd( x, i, queens ):
-      queens[x] = [x,i]
-      addQueen( x + 1, queens, n )
-    i += 1
- 
-def safeToAdd( x, y, queens ):
-  for i in range( x ):
-    if y == queens[i][1] or ( x - y ) == ( i - queens[i][1] ) or ( x + y )== ( i + queens[i][1] ):
-      return False
-  return True"""
-    
-
     
 def createDiagonals(board):
     ''' This function returns the diagonal elements for the chessboard 
@@ -66,20 +53,17 @@ def createDiagonals(board):
     diags.extend(board.diagonal(i) for i in range(board.shape[1]-1,-board.shape[0],-1)) 
     return [s for s in diags if len(s) > 1]
 
-
-
-
 def writeCNF(VariableList):
     '''
 
     '''
     global cont
     n = len(VariableList)
-    res = " "
+    res = ""
     for i in range(0,n-1):
         for j in range(1,n-i):
             cont = cont +1
-            res += "-"+str(VariableList[i]) + "-" + str(VariableList[i+j]) +" 0\n"
+            res += "-"+str(VariableList[i]) + " -" + str(VariableList[i+j]) +" 0\n"
     return res                                                   
 
 def AtMostOne(VariableList):
@@ -102,7 +86,7 @@ def main(n):
     global cont
     n = int(n)
     solution=nqueens(n)
-    header = "p cnf " + str((n*n)) +" " + str(cont+n) + "\n"
+    header = "p cnf " + str((n*n)) +" " + str(cont+2*n) + "\n"
     formula = header + solution
     text_file = open("nqueens_sol.cnf", "w")
     text_file.write(formula)
